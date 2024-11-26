9bb6a39f4e0ba4ddea413deec38b075fd187b7d6
benjamingohps <benjamingohps@gmail.com>
2024-11-26 22:40:18 +0800

    Exercise 4 done

--- exercise.py
+++ exercise.py
@@ -135,4 +135,51 @@ def calculate_dog_years():
             print('Invalid input. Please enter numbers only')
 
 # Call the function
-calculate_dog_years()
+# calculate_dog_years()
+
+
+# Exercise 4: Weather Advice
+#
+# Write a Python script named `weather_advice` that provides clothing advice based on weather conditions.
+#
+# Requirements:
+# - The script should prompt the user to enter if it is cold (yes/no).
+# - Then, ask if it is raining (yes/no).
+# - Use logical operators to determine clothing advice:
+#   - If it is cold AND raining, print "Wear a waterproof coat."
+#   - If it is cold BUT NOT raining, print "Wear a warm coat."
+#   - If it is NOT cold but raining, print "Carry an umbrella."
+#   - If it is NOT cold AND NOT raining, print "Wear light clothing."
+#
+# Hints:
+# - Use logical operators (`AND`, `OR`, `NOT`) in your if statements to handle multiple conditions.
+
+def weather_advice():
+    # Your control flow logic goes here
+    while True:
+        
+            temperature = input('Is it cold (yes/no): ').strip().lower()
+            print(f'You answered {temperature}')
+            raining = input('Is it raining? (yes/no): ').strip().lower()
+            print(f'You answered {raining}')
+
+            if temperature not in ['yes', 'no'] or raining not in ['yes', 'no']:
+                print("Invalid input. Please enter 'yes' or 'no' only.")
+                
+            elif temperature == 'yes' and raining == 'yes':
+                print('Wear a waterproof coat.')
+                break
+            elif temperature == 'yes' and raining == 'no':
+                print('Wear a warm coat.')
+                break
+            elif temperature == 'no' and raining == 'yes':
+                print('Carry an umbrella.')
+                break
+            else:
+                print('Wear light clothing')
+                break
+      
+           
+# Call the function
+weather_advice()
+
